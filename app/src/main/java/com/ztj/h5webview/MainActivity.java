8305f272cfc756dcc841566f28c1a0d7f102886d
Ztj <ztj1993@gmail.com>
2020-03-21 13:39:03 +0800

    增加了 WebView 额外处理

--- app/src/main/java/com/ztj/h5webview/MainActivity.java
+++ app/src/main/java/com/ztj/h5webview/MainActivity.java
@@ -1,11 +1,18 @@
 package com.ztj.h5webview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
 import android.widget.FrameLayout;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import io.dcloud.EntryProxy;
+import io.dcloud.common.DHInterface.ISysEventListener.SysEventType;
 import io.dcloud.feature.internal.sdk.SDK;
 
 
@@ -31,5 +38,72 @@ public class MainActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return entry_proxy.onActivityExecute(this, SysEventType.onCreateOptionMenu, menu);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        entry_proxy.onPause(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        entry_proxy.onResume(this);
+    }
+
+    @SuppressLint("WrongConstant")
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent.getFlags() != 0x10600000) {
+            // 非点击icon调用activity时才调用newintent事件
+            entry_proxy.onNewIntent(this, intent);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        entry_proxy.onStop(this);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        boolean _ret = entry_proxy.onActivityExecute(this, SysEventType.onKeyDown, new Object[]{keyCode, event});
+        return _ret ? _ret : super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        boolean _ret = entry_proxy.onActivityExecute(this, SysEventType.onKeyUp, new Object[]{keyCode, event});
+        return _ret ? _ret : super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        boolean _ret = entry_proxy.onActivityExecute(this, SysEventType.onKeyLongPress, new Object[]{keyCode, event});
+        return _ret ? _ret : super.onKeyLongPress(keyCode, event);
+    }
+
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        try {
+            int temp = this.getResources().getConfiguration().orientation;
+            if (entry_proxy != null) {
+                entry_proxy.onConfigurationChanged(this, temp);
+            }
+            super.onConfigurationChanged(newConfig);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        entry_proxy.onActivityExecute(this, SysEventType.onActivityResult, new Object[]{requestCode, resultCode, data});
     }
 }
